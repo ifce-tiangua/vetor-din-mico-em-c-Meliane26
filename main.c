0b7c401dd16f7a587b2ccf496a4e16f053c2113d
Meliane26 <maria.meliane17@gmail.com>
2024-11-19 19:05:08 -0300

    vetor dinamico

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-
+int main()
+{
+    int tamanho;
+    int *vetor;
+    
+    scanf("%d", &tamanho);
+    
+    if(tamanho == 0){
+        printf("[vazio]\n");
+        return 0;
+    }
+    
+    vetor = (int *)malloc(tamanho * sizeof(int));
+    if(vetor == NULL) {
+        printf("Erro ao alocar memoria\n");
+        return 1;
+    }
+    for(int i = 0; i < tamanho; i++){
+        scanf("%d", &vetor[i]);
+    }
+    printf("[");
+    for(int i = 0; i < tamanho; i++){
+        printf("%d", vetor[i]);
+        if(i < tamanho - 1){
+            printf(",");
+        }
+    }
+    printf("]\n");
+    
+    free(vetor);
     return 0;
 }
